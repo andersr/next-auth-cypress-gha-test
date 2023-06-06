b00f23875a9aa7890d6a8a85657f5e0084e8cc4e
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2023-06-06 11:57:33 -0400

    add cypress cloud

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 import { seedDB, deleteUser } from "./cypress/tasks";
 
 export default defineConfig({
+  projectId: '2toy3p',
   e2e: {
     setupNodeEvents(on, config) {
       on("task", {
